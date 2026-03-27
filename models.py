c3b807d3f50b27081e5ba1d6803502bacaddcdec
Hugo Degeneve <hugo.degeneve@gmail.com>
2026-03-27 15:28:29 +0100

    super pixels

--- models.py
+++ models.py
@@ -1,5 +1,15 @@
-import numpy as np
 from collections import defaultdict
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+from scipy.stats import multivariate_normal
+
+import numpy as np
+
+import utils
+import inference
 
 class Graph:
     
@@ -24,3 +34,87 @@ class Graph:
         
         self.psi[(node1, node2)] = psi12
         self.psi[(node2, node1)] = psi12.T
+
+class DensityDistance(BaseEstimator, ClassifierMixin):
+    def __init__(self, reg=1e-6):
+        self.reg = reg
+        
+        self.models = [None, None]
+
+    def fit(self, X, y):
+        """
+        X: (n_samples, 3)
+        y: (n_samples,) binary labels
+        """
+        _, n_features = X.shape
+
+        for c in range(2):
+            X_c = X[y == c]
+
+            mean = X_c.mean(axis=0)
+            cov = np.cov(X_c, rowvar=False)
+            cov += self.reg * np.eye(n_features)
+            self.models[c] = multivariate_normal(mean=mean, cov=cov)
+
+        return self
+
+    def predict_proba(self, X):
+        """
+        Returns: (n_samples, n_classes)
+        """
+        probs = []
+        
+        for model in self.models:
+            probs.append(model.pdf(X))
+
+        probs = np.stack(probs)
+        probs = probs / np.sum(probs, axis=0)
+        
+        return probs.T
+
+    def predict(self, X):
+        probs = self.predict_proba(X)
+        return np.argmax(probs, axis=1)
+
+
+class Segment():
+    
+    def __init__(self, size, model, beta=1, sigma=100):
+        self.size = size
+        self.model = model
+        self.scaler = StandardScaler()
+        
+        self.beta = beta
+        self.sigma = sigma
+    
+    def fit(self, features, labels):
+        features = self.scaler.fit_transform(features)
+        self.model.fit(features, labels)
+    
+    def predict(self, features):
+        features = self.scaler.transform(features)
+        return self.model.predict(features)
+
+    def predict_image(self, feature_map):
+        H, W, C = feature_map.shape
+        X_flat = feature_map.reshape(-1, C)
+        predictions = self.predict(X_flat).reshape(H, W, -1)
+        return predictions[..., 0]
+    
+    def __call__(self, features, max_iter):
+        graph = utils.segmentation_graph_from_image(self.model, features, self.beta, self.sigma, transform=self.scaler)
+        marginals = inference.loopy_bp(graph, max_iter)
+        mask = utils.image_from_marginals(features, marginals)
+        return mask
+
+class DensityMask(Segment):
+    
+    def __init__(self, size, beta=1, sigma=100):
+        model = DensityDistance()
+        Segment.__init__(self, size, model, beta, sigma)
+        
+class LogisticMask(Segment):
+    
+    def __init__(self, size, beta=1, sigma=100, solver="lbfgs"):
+        model = LogisticRegression(solver=solver)
+        Segment.__init__(self, size, model, beta, sigma)
--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.base import BaseEstimator
+from skimage.segmentation import mark_boundaries
+from skimage.segmentation import slic
 
 import models
 
@@ -64,7 +66,7 @@ def noise_graph_from_image(image : np.array, alpha : float, beta : float):
 
 
 def segmentation_graph_from_image(model: BaseEstimator, image: np.ndarray,
-                                  beta: float, sigma: float, scaler=None):
+                                  beta: float, sigma: float, transform=None):
     H, W, C = image.shape
     N = H * W
     graph = models.Graph(2)
@@ -73,8 +75,8 @@ def segmentation_graph_from_image(model: BaseEstimator, image: np.ndarray,
 
     X_flat = image.reshape(-1, C)
     
-    if scaler is not None:
-        X_flat = scaler.transform(X_flat)
+    if transform is not None:
+        X_flat = transform.transform(X_flat)
     
     probas = model.predict_proba(X_flat).reshape(H, W, -1)
     probas /= probas.sum(axis=-1, keepdims=True) + 1e-10
@@ -114,3 +116,90 @@ def image_from_marginals(image, marginals):
         im[i, j] = 0 if x < 0.5 else 1
     
     return im
+
+
+def get_superpixels(image, n_segments=200, compactness=10, start_label=0):
+    segments = slic(np.array(image), n_segments=n_segments, compactness=compactness, start_label=start_label)
+
+    pixels = np.unique(segments)
+    edges_h = np.stack([segments[:, :-1], segments[:, 1:]], axis=-1)
+    edges_h = edges_h[edges_h[:, :, 0] != edges_h[:, :, 1]]
+    edges_v = np.stack([segments[:-1, :], segments[1:, :]], axis=-1)
+    edges_v = edges_v[edges_v[:, :, 0] != edges_v[:, :, 1]]
+    edges = np.vstack((edges_h.reshape(-1, 2), edges_v.reshape(-1, 2)))
+    edges = np.sort(edges, axis=1)
+    edges = np.unique(edges, axis=0)
+    
+    return segments, pixels, edges
+
+def process_super_pixel(image, mask, segments, pixels, threshold=0.5):
+    
+    if mask is not None:
+        mask = np.array(mask)
+        labels = np.stack([mask[segments == p].mean() for p in pixels]) > threshold
+        super_mask = np.zeros_like(mask)
+        for i, p in enumerate(pixels):
+            super_mask[segments == p] = labels[i]
+    else:
+        labels = None
+        super_mask = None
+    if image is not None:
+        image = np.array(image)
+        features = np.stack([image[segments == p].mean(axis=0) for p in pixels])
+    else:
+        features = None
+    
+    return features, labels, super_mask
+
+def get_super_pixels_graph(model, image, beta, n_segments=200, compactness=10, start_label=0, transform=None):
+    graph = models.Graph(2)
+    
+    pairwise = np.exp(-np.array([[0, beta],
+                                 [beta, 0]]))
+    segments, pixels, edges = get_superpixels(image, n_segments, compactness, start_label)
+    
+    X, _, _ = process_super_pixel(image, None, segments, pixels)
+    
+    if transform is not None:
+        X = transform.transform(X)
+    
+    probas = model.predict_proba(X)
+    probas /= probas.sum(axis=-1, keepdims=True) + 1e-10
+
+    for i, k in enumerate(pixels):
+        graph.add_node(k, probas[i])
+    
+    for k1, k2 in edges:
+        graph.add_edge(int(k1), int(k2), pairwise)
+    
+    return graph
+
+def compute_centroids(segments):
+    centroids = {}
+    
+    for label in np.unique(segments):
+        coords = np.column_stack(np.where(segments == label))
+        y, x = coords.mean(axis=0)  # careful: (row, col) = (y, x)
+        centroids[label] = (x, y)   # switch to (x, y) for plotting
+        
+    return centroids
+
+def plot_superpixels(image, segments, edges):
+    centroids = compute_centroids(segments)
+
+    plt.figure(figsize=(8, 8))
+    plt.imshow(image)
+ 
+    for u, v in edges:
+        x1, y1 = centroids[u]
+        x2, y2 = centroids[v]
+        plt.plot([x1, x2], [y1, y2], 'b-', linewidth=1)
+
+    xs = [centroids[l][0] for l in centroids]
+    ys = [centroids[l][1] for l in centroids]
+    plt.scatter(xs, ys, c='red', s=10)
+    
+    plt.imshow(mark_boundaries(image, segments))
+
+    plt.axis('off')
+    plt.show()
